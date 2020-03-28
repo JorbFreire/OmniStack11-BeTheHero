365d868a16b9afac1423f19e62a23a772e79201e
JorbFreire <jorbfreire@ufrn.edu.br>
2020-03-28 11:17:13 -0300

    navigation

--- mobile/App.js
+++ mobile/App.js
@@ -5,23 +5,10 @@ import {
   View
 } from 'react-native';
 
+import Routes from './src/routes';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
-    </View>
+    <Routes />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  text: {
-    color: '#fff',
-  }
-});
